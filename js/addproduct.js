73fc13b04fff97c781ccee2186dfa26f120ffb4e
Kevin McGrory <kevin@kevinmcgrory.com>
2018-06-04 15:59:19 -0400

    completed JS file used before testing

--- js/addproduct.js
+++ js/addproduct.js
@@ -1,1 +1,52 @@
 var myRef= new Firebase('https://demoapp-984c6.firebaseio.com/');
+
+var itemName;
+var itemDescription;
+var itemPrice;
+var baseImg;
+
+$('#imageInput').change(function(){
+	var reader= new FileReader();
+	reader.onloadend=function(){
+		baseImg=reader.result;
+		$('#preview').attr('src',reader.result);		
+	};
+	reader.readAsDataURL(this.files[0]);
+
+});
+
+$('#addForm').change(function(){
+	itemName=$('#itemName').val();
+	itemDescription=$('#itemDescription').val();
+	itemPrice=$('#itemPrice').val();
+
+	if(itemName&&itemDescription&&itemPrice){
+		$('#saveButton').removeProp('disabled');
+	}
+	else{
+		$('#saveButton').prop('disabled','disabled');
+	}		
+});
+
+function addProduct(){
+	itemName=$('#itemName').val();
+	itemDescription=$('#itemDescription').val();
+	itemPrice=$('#itemPrice').val();
+
+	if(!baseImg){
+		baseImg='NONE';
+	}
+
+	var prodRef=myRef.child('products');
+
+	// passes onject to Firebase to save
+	prodRef.set({
+		name: itemName,
+		description: itemDescription,
+		price: itemPrice,
+		image: baseImg
+	});
+
+}
+
+$('#saveButton').click(addProduct);
