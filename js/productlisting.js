7a175c626e2d32edfc9cb5da196d2a0866c0b7b3
Kevin McGrory <kevin@kevinmcgrory.com>
2018-06-05 16:50:33 -0400

    finished first pass on productlisting.gs

--- js/productlisting.js
+++ js/productlisting.js
@@ -4,4 +4,55 @@ var prodData={};
 
 theRef.on('value',function(snap)){
 	
-}
+	prodData=snap.val();
+
+	$.each(prodData, function(index, value){
+		var prodPreview = '<div class="row">';
+		prodPreview += '<div class="col-md-3 prodListHeader">';
+		prodPreview += '<h2>' + value.name + '</h2>';
+		prodPreview += '</div>';
+
+		prodPreview += '<div class="col-md-3 prodListHeader">';
+		prodPreview += '<h2>' + value.price + '</h2>';
+		prodPreview += '</div>';		
+
+		prodPreview += '</div>';
+
+		prodPreview += '<div class="row">';
+
+		prodPreview += '<div class="col-md3 picFix">';	
+		
+		if (value.image=='NONE') {
+			prodPreview += '<img alt="No Pic">';
+		} else {
+			prodPreview += '<img src=' + value.image + '>';
+		}
+		prodPreview += '</div>';
+
+		prodPreview += '<div class="col-md3">';	
+		prodPreview += '<p>' + value.description + '</p>';
+		prodPreview += '</div>';
+		prodPreview += '</div>';
+
+		prodPreview += '<div class="row>';
+
+		prodPreview += '<div class="col-md3">';			
+		prodPreview += '<button type="button" class="btn btn-warning" onclick="editProd(\'' + index + '\')">EDIT PRODUCT</button>';
+		prodPreview += '</div>';		
+
+		prodPreview += '<div class="col-m9">';			
+		prodPreview += '<button type="button" class="btn btn-danger" onclick="editProd(\'' + index + '\')">DELETE PRODUCT</button>';
+		prodPreview += '</div>';		
+
+		prodPreview += '</div>';	
+
+		prodPreview += '<div class="row spacer">';			
+		prodPreview += '</div>';	
+
+		$(prodPreview).appendTo('#main')
+
+	});
+
+}, function(errorObject){
+	console.log('The read failed: '+errorObject.code);
+});
