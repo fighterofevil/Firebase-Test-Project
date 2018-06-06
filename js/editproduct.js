a5669d76d1f50c2edfd3bdf99ee7aa38423fc350
Kevin McGrory <kevin@kevinmcgrory.com>
2018-06-06 12:50:11 -0400

    pushing up confirmed working code for whole project

--- js/editproduct.js
+++ js/editproduct.js
@@ -1,4 +1,4 @@
-var prodID=window.name;
+var prodID = window.name;
 
 var prodRef= new Firebase('https://demoapp-984c6.firebaseio.com/products');
 
@@ -6,55 +6,57 @@ var name, desc, price, img;
 
 var oneProduct = {};
 
-
-// fireebase method
 prodRef.child(prodID).once('value', function(snapshot){
-	oneProduct = snapshot.val();
+    oneProduct = snapshot.val();
+    name = oneProduct.name;
+    desc = oneProduct.description;
+    price = oneProduct.price;
+    img = oneProduct.image;
 
-	name = oneProduct.name;
-	desc = oneProduct.description;
-	price = oneProduct.price;
-	img = oneProduct.image;
-
-	$('#itemName').val(name);
-	$('#itemDescription').val(desc);
-	$('itemPrice').val(price);
-
-	$('#preview').attr('src',img);
-
+    $('#itemName').val(name);    
+    $('#itemDescription').val(desc);
+    $('#itemPrice').val(price);
+    
+    $('#preview').attr('src', img);
 });
 
-$('#imageInput').change(function(){
-	var reader = new FileReader();
-	reader.onloadend=function(){
-		img=reader.result;
-		$('#preview').attr('src',reader.result);	
-	};
-	reader.readAsDataURL(this.files[0]);
-});
 
-function onComplete(error){
-	if (error) {
-		alert('update failed, error code : ' + error.code);
-	} else {
-		alert('update suceeded');
-		location.assign('productlisting.html');
-	}
+$('#imageInput').change(function() {
+    var reader = new FileReader();    
+    reader.onloadend = function() {
+        img = reader.result;
+        $('#preview').attr('src', reader.result); 
+    };
+   reader.readAsDataURL(this.files[0]);
+}); 
+
+
+function onComplete (error) {
+    if (error) {
+        alert('update failed, error code : ' + error.code);
+    } else {
+        alert('update suceeded');
+        location.assign('productListing.html');
+    }
 }
 
-function editProduct(){
-	var editName = $('#itemName').val();
-	var editDesc = $('#itemDescription').val();
-	var editPrice = $('#itemPrice').val();
-	var editImg = img;
+function editProduct() {
+    var editName = $('#itemName').val();  
+    var editDesc = $('#itemDescription').val();
+    var editPrice = $('#itemPrice').val();
+    var editImg = img;
+    
+    
+    prodRef.child(prodID).update({
+        name: editName,
+        description: editDesc,
+        price: editPrice,
+        image: editImg
 
-	// fireebase call
-	prodRef.child(prodID).update({
-		name: editName,
-		description: editDesc,
-		price: editPrice,
-		image: editImg
-	},
-		onComplete
-	);
+    },
+        onComplete
+    );    
+    
 }
+
+$('#editButton').click(editProduct);
--- js/productlisting.js
+++ js/productlisting.js
@@ -1,80 +1,95 @@
 var theRef= new Firebase('https://demoapp-984c6.firebaseio.com/products');
 
-function editProd(id){
-	//unsecure method of passing data as stated in demo.  this is done as a hack workaround since there is no feature set such as routing. 
-	window.name=id;
-	location.assign('editproduct.html');
+function logOff() {
+    theRef.unauth();
+    location.assign('index.html');
 }
 
-function onComplete(error){
-	if (error) {
-		alert('Delete failed!');
-	} else {
-		// alert('Delete product');
-		location.reload(true);
-	}
+
+var authData = theRef.getAuth();
+
+
+function editProd(id) { 
+    window.name = id;
+    location.assign('editProduct.html');
+
 }
 
-function deleteProd(id){
-	if(confirm('Are you sure you want to delete!?')==true){
-		// firebase call
-		theRef.child(id).remove(onComplete);
-	}
+function onComplete(error) {
+    if (error){
+        alert('Delete Failed!');
+    } else {
+        //alert('Deleted product!');
+        location.reload(true);
+    }
 }
 
-var prodData={};
+function deleteProd(id) {
+    if (confirm('Are you sure you want to delete!?') == true) {
+       theRef.child(id).remove(onComplete);
+    } 
+}
 
-theRef.on('value',function(snap){
-	
-	prodData=snap.val();
 
-	$.each(prodData, function(index, value){
-		var prodPreview = '<div class="row">';
-		prodPreview += '<div class="col-md-3 prodListHeader">';
-		prodPreview += '<h2>' + value.name + '</h2>';
-		prodPreview += '</div>';
+var prodData = {};
 
-		prodPreview += '<div class="col-md-3 prodListHeader">';
-		prodPreview += '<h2>' + value.price + '</h2>';
-		prodPreview += '</div>';		
+theRef.on('value', function(snap) {
 
-		prodPreview += '</div>';
+    prodData = snap.val();
+    
+    $.each(prodData, function(index, value) {
+        var prodPreview = '<div class="row">';
+        
+        prodPreview += '<div class="col-md-3 prodListHeader">';    
+        prodPreview += '<h2>' + value.name + '</h2>';
+        prodPreview += '</div>';
+        
+        prodPreview += '<div class="col-md-3 prodListHeader">';
+        prodPreview += '<h2>$' + value.price + '</h2>';
+        prodPreview += '</div>';
+        
+        prodPreview += '</div>';
+        
+        
+        prodPreview += '<div class="row">';
+        
+        prodPreview += '<div class="col-md-3 picFix">';
+        if (value.image == 'NONE') {
+            prodPreview += '<img alt="No Pic">';
+        } else {
+            prodPreview += '<img src=' + value.image + '>';
+        }
+        prodPreview += '</div>';
+        
+        
+        prodPreview += '<div class="col-md-3">';
+        prodPreview += '<p>' + value.description + '</p>';
+        prodPreview += '</div>';
+        prodPreview += '</div>';
+        
+        
+        prodPreview += '<div class="row">';
+        
+        prodPreview += '<div class="col-md-3">';
+        prodPreview += '<button type="button" class="btn btn-warning" onclick="editProd(\'' + index + '\')">EDIT PRODUCT</button>';
+        prodPreview += '</div>';
+        
+        prodPreview += '<div class="col-md-9">';
+        prodPreview += '<button type="button" class="btn btn-danger" onclick="deleteProd(\'' + index + '\')">DELETE PRODUCT</button>';
+        prodPreview += '</div>';
+        
+        prodPreview += '</div>';
+        
+        prodPreview += '<div class="row spacer">';
+        prodPreview += '</div>';
+        
+        $(prodPreview).appendTo('#main');
 
-		prodPreview += '<div class="row">';
+    
+    });
 
-		prodPreview += '<div class="col-md3 picFix">';	
-		
-		if (value.image=='NONE') {
-			prodPreview += '<img alt="No Pic">';
-		} else {
-			prodPreview += '<img src=' + value.image + '>';
-		}
-		prodPreview += '</div>';
+}, function(errorObject) {
+    
+console.log('The read failed: ' + errorObject.code);  
 
-		prodPreview += '<div class="col-md3">';	
-		prodPreview += '<p>' + value.description + '</p>';
-		prodPreview += '</div>';
-		prodPreview += '</div>';
-
-		prodPreview += '<div class="row>';
-
-		prodPreview += '<div class="col-md3">';			
-		prodPreview += '<button type="button" class="btn btn-warning" onclick="editProd(\'' + index + '\')">EDIT PRODUCT</button>';
-		prodPreview += '</div>';		
-
-		prodPreview += '<div class="col-m9">';			
-		prodPreview += '<button type="button" class="btn btn-danger" onclick="editProd(\'' + index + '\')">DELETE PRODUCT</button>';
-		prodPreview += '</div>';		
-
-		prodPreview += '</div>';	
-
-		prodPreview += '<div class="row spacer">';			
-		prodPreview += '</div>';	
-
-		$(prodPreview).appendTo('#main')
-
-	});
-
-}, function(errorObject){
-	console.log('The read failed: '+errorObject.code);
 });
